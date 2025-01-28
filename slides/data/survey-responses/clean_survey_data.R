61509265b5bddef91832bd90bccd14f412c47f34
tgerarden <tgerarden@users.noreply.github.com>
2025-01-28 14:16:34 +0000

    deploy: 9ec2a651ad0424b16e0c728c27da633c2a184b61

--- slides/data/survey-responses/clean_survey_data.R
+++ slides/data/survey-responses/clean_survey_data.R
@@ -2,16 +2,16 @@ library(tidyverse)
 library(rjson)
 
 # IMPORT SURVEY DATA ----
-all_responses <- read_csv("content/slides/data/survey-responses/lab-1-survey.csv") |>
-  separate(netid, into = c("netid"), sep = "@") |>
+all_responses <- read_csv("content/slides/data/survey-responses/homework-1-survey.csv") |>
+  separate(netid, into = c("netid"), sep = "@", extra = "drop") |>
   mutate(netid = tolower(netid))
 
 responses <- all_responses
 
 enrolled_students <-
   rbind(
-    read_csv("content/slides/data/survey-responses/enrollment-feb12-sec1.csv"),
-    read_csv("content/slides/data/survey-responses/enrollment-feb12-sec2.csv")
+    read_csv("content/slides/data/survey-responses/enrollment-sec1-jan27.csv"),
+    read_csv("content/slides/data/survey-responses/enrollment-sec2-jan27.csv")
     ) |>
   select(netid = `NET ID`)
 
@@ -48,6 +48,7 @@ concentrations_raw <- responses |>
   mutate(
     program = case_when(
       str_detect(degree, "BA") ~ "BA",
+      str_detect(degree, "Bachelor of Arts") ~ "BA",
       str_detect(degree, "BS|AEM|Dyson") ~ "BS",
       str_detect(degree, "Applied Economics and Management") ~ "BS",
       str_detect(degree, "MPS") ~ "MPS",
@@ -56,8 +57,20 @@ concentrations_raw <- responses |>
       str_detect(degree, "MS") ~ "MS",
       str_detect(degree, "PhD") ~ "PhD",
       str_detect(degree, "CAS") ~ "BA",
-      TRUE ~ "BS"
+      str_detect(degree, "MILR") ~ "MILR",
+      str_detect(degree, "MMH") ~ "MMH",
+      str_detect(degree, "MA") ~ "MA",
+      str_detect(degree, "bs") ~ "BS",
+      TRUE ~ NA
+      # TRUE ~ "BS"
     )
+  ) |>
+  # coarse definition
+  mutate(
+    program = case_when(
+      str_detect(program, "^B") ~ "Bachelor's",
+      str_detect(program, "^M") ~ "Master's"
+           )
   )
 
 concentrations <- concentrations_raw |>
@@ -67,14 +80,15 @@ concentrations <- concentrations_raw |>
          dev = str_detect(concentration, "development"),
          econ = str_detect(concentration, "economics") | str_detect(degree, "Econ"),
          # entr = str_detect(concentration, "entrepreneurship"),
-         enviro = str_detect(concentration, "sustainable|sustainability|environmental|eere|sbep"),
+         enviro = str_detect(concentration, "sustainable|sustainability|environmental|eere|sbep|sbee"),
          finance = str_detect(concentration, "finance") | str_detect(concentration, "finace"), # BS finance and MPS behavioral finance
          food = str_detect(concentration, "food"),
          mktg = str_detect(concentration, "marketing"),
          mgmt = str_detect(concentration, "management") | concentration=="tm", # management + tech management + agribusiness management
-         strategy = str_detect(concentration, "strategy")
+         strategy = str_detect(concentration, "strategy"),
+         hr = str_detect(concentration, "human resources|hr ")
          ) |>
-  mutate(other = (ba + acct + dev + econ + enviro + finance + food + mktg + mgmt + strategy)==0)
+  mutate(other = (ba + acct + dev + econ + enviro + finance + food + mktg + mgmt + strategy + hr)==0)
 
 
 # COMPANIES ----
@@ -83,29 +97,27 @@ companies <- responses |>
   mutate(
     ticker = case_match(
       ticker,
-      "Not sure" ~ NA,
-      "I don't have just one" ~ NA,
-      "none" ~ NA,
-      "don't have any" ~ NA,
-      "apple" ~ "AAPL",
-      "$AMD" ~ "AMD",
-      "APPL" ~ "AAPL",
-      "SPINN" ~ "SPINN.HE",
-      "PFE (Pfizer Inc.)" ~ "PFE",
-      "Tesla" ~ "TSLA",
-      "Tesla, Apple, BYD" ~ "TSLA",
-      "I don't really have a favorite, but I'd probably go with MSFT" ~ "MSFT",
-      "Tough one... I'd say $NEE" ~ "NEE",
-      "ISIN: US88160R1014" ~ "TSLA",
+      "TSLA/NVDA" ~ "TSLA",
       "None" ~ NA,
-      "not sure" ~ NA,
-      "NASDAQ" ~ "NDAQ",
-      "HBD, AAPL, TCS" ~ "AAPL",
-      "PayPal" ~ "PYPL",
-      "$AAPL" ~ "AAPL",
+      "DOCS (London Stock Exchange)" ~ "DOCS",
+      "CVS Health" ~ "CVS",
+      "AAPL, GOOGL" ~ "AAPL",
+      "NVIDIA Corp" ~ "NVDA",
+      "N/A" ~ NA,
       "Apple" ~ "AAPL",
-      "no" ~ NA,
-      "BOM:500800" ~ "TATACONSUM",
+      "APL" ~ "AAPL",
+      "APPL" ~ "AAPL",
+      "I didn't learnt much about that." ~ NA,
+      "Do not really have one" ~ NA,
+      "DIS, UTHR" ~ "DIS",
+      "NASDAQ: FLNC" ~ "FLNC",
+      "DIS (Walt Disney Co)" ~ "DIS",
+      "It's hard question, as I don't have any favorite comes to mind right now. Maybe, Netflix Inc." ~ "NFLX",
+      "Nvidia" ~ "NVDA",
+      "227.44 Apple" ~ "AAPL",
+      "Don't have any" ~ NA,
+      "NASDAQ:AAPL" ~ "AAPL",
+      "Estee Lauder Companies Inc.  (EL)" ~ "EL",
       .default = ticker
     )
   )
@@ -120,14 +132,16 @@ companies <- left_join(companies, tickers, join_by(ticker)) |>
   mutate(
     name = case_match(
       ticker,
-      "CICC" ~ "China International Capital Corp",
-      "601318" ~ "Ping An Insurance",
-      "IKE" ~ "ikeGPS",
+      "RIL" ~ "Reliance Industries Ltd",
+      "TTM" ~ "Titan Minerals Ltd",
+      "002594" ~ "BYD",
+      "TATAMOTORS" ~ "Tata Motors Ltd",
       .default = name
     )
   )
 
-
+# check for missing company names
+companies |> filter(is.na(name) & !is.na(ticker))
 
 
 # SAVE DATA ----
@@ -142,22 +156,22 @@ save(prior_programming,
 #   write_csv(paste0(dir,"/student_info.csv"))
 
 
-# MAKE LIST OF COMPANIES FOR GROUP PROJECT ----
-load("rstudio-cloud/group-project/sp500.RData")
-
-companies |>
-  rename(symbol = ticker,
-         sec_name = name) |>
-  group_by(symbol, sec_name) |>
-  count() |>
-  ungroup() |>
-  left_join(sp500_companies |> select(symbol, company),
-            join_by(symbol)) |>
-  mutate(name = case_when(
-    is.na(company) ~ str_to_title(sec_name),
-    TRUE ~ company
-  )) |>
-  select(name, n) |>
-  filter(!is.na(name)) |>
-  arrange(name) |>
-  write_csv("rstudio-cloud/group-project/our-companies.csv")
+# # MAKE LIST OF COMPANIES FOR GROUP PROJECT ----
+# load("rstudio-cloud/group-project/sp500.RData")
+#
+# companies |>
+#   rename(symbol = ticker,
+#          sec_name = name) |>
+#   group_by(symbol, sec_name) |>
+#   count() |>
+#   ungroup() |>
+#   left_join(sp500_companies |> select(symbol, company),
+#             join_by(symbol)) |>
+#   mutate(name = case_when(
+#     is.na(company) ~ str_to_title(sec_name),
+#     TRUE ~ company
+#   )) |>
+#   select(name, n) |>
+#   filter(!is.na(name)) |>
+#   arrange(name) |>
+#   write_csv("rstudio-cloud/group-project/our-companies.csv")
